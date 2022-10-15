9f45558abb0697bca4df95f1a11621ce0c9c29c3
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-10-14 18:33:09 -0700

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+  end
+
+  def match list
+    list.select do |w| 
+      if(@word.chars.sort == w.chars.sort)
+        w
+      end
+    end
+  end
+end
